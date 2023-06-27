c6f8e1b7cf98af1d956f0f9565d435908f8b1a31
Sanjay <sanjayacharya992@.com>
2023-06-27 23:20:50 +0530

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("hello world")
+})
+
+app.get("/home", (req,res)=>{
+    res.send("hello home")
+})
+
+app.get("/about", (req,res)=>{
+    res.send("hello about")
+})
+
+app.listen(5000,()=>{
+    console.log("loading")
+})
